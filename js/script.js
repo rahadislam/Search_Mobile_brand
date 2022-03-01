5fb798a22b39ab327896ff4d23e1055f015d58bf
rahadislam <rahadislm.web@gmil.com>
2022-03-01 16:10:30 +0600

    api done

--- js/script.js
+++ js/script.js
@@ -8,21 +8,28 @@ const scarchPhone=()=>{
 }
 
 const showProduct=products=>{
-    console.log(products);
     for(let product of products.data){
-        console.log(product);
         const showallProduct=document.getElementById('showAllproduct');
         const div=document.createElement('div');
         div.innerHTML=`
         <div class="py-8 bg-slate-50 rounded-2xl drop-shadow-lg">
-        <img class="w-60 mx-auto" src="${product.image}" alt="">
+        <img class="w-auto mx-auto" src="${product.image}" alt="">
         <div class="text-center">
         <h1 class="font-bold py-2">Name:${product.phone_name}</h1>
         <span class="block py-2">Brand:${product.brand}</span>
-        <button class="bg-sky-900 text-white rounded-lg py-3 px-4">Details</button>
+        <button onclick="showDetails('${product.slug}')" class="bg-sky-900 text-white rounded-lg py-3 px-4">Details</button>
         </div>
     </div>`;
     showallProduct.appendChild(div);
 
     }
+}
+
+const showDetails=details=>{
+    
+    const url=`https://openapi.programming-hero.com/api/phone/${details}`
+    fetch(url)
+    .then(res =>res.json())
+    .then(data =>console.log(data))
+
 }
